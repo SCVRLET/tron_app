fa703b893ca5381d968ee257c6d46c1e216b813d
scvrlet <55vlad.boch55@gmail.com>
2025-03-31 11:50:02 +0500

    making api_test work with test db

--- app/tests/api_test.py
+++ app/tests/api_test.py
@@ -1,19 +1,17 @@
 import pytest
 
-import requests
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.settings import CURRENT_APP_HOST
 
+from app.wallets.routes import get_wallet_data
 
-def test_get_wallet_with_address():
-    r = requests.post(f'{CURRENT_APP_HOST}/wallet/',
-                      json={'address': 'TLa2f6VPqDgRE67v1736s7bJ8Ray5wYjU7'})
+from app.wallets.schemas import WalletAddressRequest, Wallet
 
-    assert r.status_code == 200
-    
-    data = r.json()
 
-    assert data['address'] == 'TLa2f6VPqDgRE67v1736s7bJ8Ray5wYjU7'
-    assert 'balance' in data
-    assert 'energy' in data
-    assert 'bandwidth' in data
+@pytest.mark.asyncio(loop_scope="session")
+async def test_get_wallet_with_address(async_db_session: AsyncSession):
+    wallet = await get_wallet_data(WalletAddressRequest(address='TLa2f6VPqDgRE67v1736s7bJ8Ray5wYjU7'), db=async_db_session)
+
+    assert wallet.address == 'TLa2f6VPqDgRE67v1736s7bJ8Ray5wYjU7'
+    assert Wallet.model_validate(wallet)
